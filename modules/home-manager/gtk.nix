9e7001b6f2fe9a0b83dd8375c742621140645320
NikSne <commits@niksne.ru>
2024-07-01 19:56:30 +0300

    Remove gtk deprecation notice Lot of people use gtk module

--- modules/home-manager/gtk.nix
+++ modules/home-manager/gtk.nix
@@ -98,16 +98,18 @@ in
   ];
 
   config = mkMerge [
-    (mkIf (enable || cfg.gnomeShellTheme) {
-      warnings = [
-        ''
-          `gtk.catppuccin.enable` and `gtk.catppuccin.gnomeShellTheme` are deprecated and will be removed in a future release.
+    # That modul is archived but I need it
 
-          The upstream port has been archived and support will no longer be provided.
-          Please see https://github.com/catppuccin/gtk/issues/262
-        ''
-      ];
-    })
+    # (mkIf (enable || cfg.gnomeShellTheme) {
+    #   warnings = [
+    #     ''
+    #       `gtk.catppuccin.enable` and `gtk.catppuccin.gnomeShellTheme` are deprecated and will be removed in a future release.
+
+    #       The upstream port has been archived and support will no longer be provided.
+    #       Please see https://github.com/catppuccin/gtk/issues/262
+    #     ''
+    #   ];
+    # })
 
     (mkIf enable {
       gtk.theme =
